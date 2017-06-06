c5dba51e263a4e0d76baa8a9d6a5e8cf4713bca6
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-06-07 00:14:15 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,185 +1,146 @@
-#!/bin/bash
+#!/usr/bin/env bash 
 
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 
-update() {
-  git pull
-  git submodule update --init --recursive
-  install_rocks
+logo() {
+    declare -A logo
+    seconds="0.004"
+logo[0]="  .          '||    ||' '||'''|  '||    ||' '||'|.  '||'''|  '||''|."
+logo[1]=".||.   ... .  |||  |||   || .     |||  |||   ||  ||  || .     ||   ||"
+logo[2]=" ||   || ||   |'|..'||   ||'|     |'|..'||   ||''|.  ||'|     ||''|'"
+logo[3]=" ||    |''    | '|' ||   ||       | '|' ||   ||   || ||       ||   |."
+logo[4]=" '|.' '|||.  .|. | .||. .||....| .|. | .||. .||..|' .||....| .||.  '|'"
+logo[5]="    .|...'"
+logo[6]="➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖"
+logo[7]="Channel : @tgMember"
+logo[8]="Develop by @sajjad_021"
+printf "\e[38;5;213m\t"
+    for i in ${!logo[@]}; do
+        for x in `seq 0 ${#logo[$i]}`; do
+            printf "${logo[$i]:$x:1}"
+            sleep $seconds
+        done
+        printf "\n\t"
+    done
+printf "\n"
 }
-#By: @sajjad_021
-# Will install luarocks on THIS_DIR/.luarocks
+
+log() {
+  echo -e "\033[38;5;105m .               /|\,/ |\,  ,- _~,   /\|,/ \|,   - _,,     ,- _~,  -_ /\,\033[0;00m"
+  echo -e "\033[38;5;142m||    _          /| || ||   (' /| /  /| || ||     -/  )   (' /| /   || ,,\033[0;00m"
+  echo -e "\033[38;5;099m=||=  / \\        || || ||  ((  ||/=  || || ||    ~||_<   ((  || =  /|| /|.\033[0;00m"
+  echo -e "\033[38;5;034m||  || ||        ||=|= ||  ((  ||    ||=|= ||     || |\  (( |||    \||/-\,\033[0;00m"
+  echo -e "\033[38;5;406m||  || ||       ~|| || ||   ( / |   ~|| || ||     ,/--||  (   |     ||  \.,\033[0;00m"
+  echo -e "\033[38;5;129m||  \|_-|        |, |\,|\,   -____-  |, |\,|\,   _--_-'    -____- _---_-|.\033[0;00m"
+         echo -e "\033[38;5;129m||   -_-_\033[0;00m"
+            echo -e "\033[38;5;129m,_-_-\.\033[0;00m"
+ }
+
 install_luarocks() {
+ echo -e "\e[38;5;142mInstalling LuaRocks\e"
   git clone https://github.com/keplerproject/luarocks.git
   cd luarocks
-  git checkout tags/v2.2.1 # Current stable
+  git checkout tags/v2.4.2 # Current stable
 
   PREFIX="$THIS_DIR/.luarocks"
 
   ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
-
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  make build && make install
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting.";exit $RET;
-  fi
-
-  cd ..
+   make build && make install
+cd ..
   rm -rf luarocks
 }
 
 install_rocks() {
-  ./.luarocks/bin/luarocks install luasocket
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install oauth
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install redis-lua
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lua-cjson
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install fakeredis
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install xml
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install feedparser
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install serpent
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
+echo -e "\e[38;5;105mInstall rocks service\e"
+rocks="luasocket luasec redis-lua lua-term serpent dkjson Lua-cURL multipart-post lanes xml fakeredis luaexpat luasec lbase64 luafilesystem lub lua-cjson feedparser serpent"
+    for rock in $rocks; do
+      ./.luarocks/bin/luarocks install $rock
+    done
+}
+  
+tg() {
+echo -e "\e[38;5;099minstall telegram-cli\e"
+    rm -rf ../.telegram-cli
+    cd libs
+    wget https://valtman.name/files/telegram-cli-1222
+    mv telegram-cli-1222 telegram-cli
+    chmod +x telegram-cli
+cd ..
+}
+  
+install2() {
+echo -e "\e[38;5;034mInstalling more dependencies\e"
+    sudo apt-get install screen -y
+    sudo apt-get install tmux -y
+    sudo apt-get install upstart -y
+    sudo apt-get install libstdc++6 -y
+    sudo apt-get install lua-lgi -y
+    sudo apt-get install libnotify-dev -y
 }
 
-install2() {
-cd libs
-  	sudo apt-get update
-		sudo apt-get upgrade
-	  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+py() {
+ sudo apt-get install python-setuptools python-dev build-essential 
+ sudo easy_install pip
+ sudo pip install redis
+}
+
+install() {
+echo -e "\e[38;5;035mUpdating packages\e"
+   sudo apt-get update -y
+   sudo apt-get upgrade -y
+
+echo -e "\\e[38;5;129mInstalling dependencies\e"
+	 sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+		sudo apt-get update
+	  sudo apt-get upgrade
+    sudo apt-get dist-upgrade
 		sudo apt-get install g++-4.7 -y c++-4.7 -y
 		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
 		sudo apt-get install screen -y
 		sudo apt-get install tmux -y
 		sudo apt-get install libstdc++6 -y
 		sudo apt-get install lua-lgi -y
-		sudo apt-get install libnotify-dev -y
-  sudo service redis-server restart
-	wget http://valtman.name/files/telegram-cli-1222
-  mv telegram-cli-1222 telegram-cli
-   chmod +x telegram-cli
-   chmod +x launch.sh
-   chmod +x anticrash.sh
-  cd ..
-  install_luarocks
-  install_rocks
-  exit 1
+		sudo apt-get install libnotify-dev -y	
+               	sudo apt-get update
+	        sudo apt-get upgrade
+		install_luarocks
+		sudo apt-get update
+	  sudo apt-get upgrade
+	  install2
+install_luarocks
+install_rocks
+py
+tg
+log
+echo -e "\e[38;5;046mInstalling packages successfully\033[0;00m"
 }
 
-sudos() {
-  read -p "Do you want to update the project? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        sudo apt-get update
-	    wait
-	    sudo apt-get upgrade
-	    wait
-	    sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
-	    wait
-		install2
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        install2
-	else
-	    sudos
-    fi
-}
-
-install() {
-  read -p "Do you want to update the project? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        sudo apt-get update
-	    wait
-	    sudo apt-get upgrade
-	    wait
-	    sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
-	    wait
-		install2
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        install2
-	else
-	    sudos
-    fi
-}
-
-que() {
-read -p "Do you want to install starter files? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        install
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        exit 1
-	else
-	    que
-fi
-}
-
-error() {
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion
-}
-
-#By: @sajjad_021
 if [ "$1" = "install" ]; then
-  install
-elif [ "$1" = "update" ]; then
-  update
-elif [ "$1" = "error" ]; then
-  error
-else
-  if [ ! -f ./libs/telegram-cli ]; then
-    echo -e "\033[38;5;208mError! Tg not found, Please reply to this message:\033[0;00m"
-    read -p "Do you want to install starter files? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        install
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        exit 1
-	else
-	    que
-    fi
-  fi
-
-  if [ ! -f ./libs/telegram-cli ]; then
-    echo -e "\033[38;5;208mError! Tg not found, Please reply to this message:\033[0;00m"
-    read -p "Do you want to install starter files? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        install
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        exit 1
-	else
-	    que
-    fi
+install
 fi
+
+if [ "$1" = "api" ]; then
+while true; do
+screen -S nohup lua api.lua
+done
+fi
+
+if [ "$1" = "bot" ]; then
 while(true) do
    #sudo service redis-server restart
 	./libs/telegram-cli -s ./bot/bot.lua $@
 done
+fi
+
+if [ ! -f ./tg/telegram-cli ]; then
+    echo -e "\033[38;5;208mError! telegram-cli not found, Please reply to this message:\033[1;208m"
+    read -p "Do you want to install and config? [y/n] = "
+	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
+	chmod 777 make.sh
+        install
+    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
+        exit 1
+  fi
 fi
